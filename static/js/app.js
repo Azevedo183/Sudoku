513ca77d97df009f4dd8e06118154028b04e8ee3
Azevedo183 <armandojpazevedo@gmail.com>
2024-07-16 20:01:47 +0100

    Check Error

--- static/js/app.js
+++ static/js/app.js
@@ -24,6 +24,7 @@ let seconds = 0;
 let su = undefined;
 let su_answer = undefined;
 
+let selected_cell = -1;
 
 
 
@@ -109,6 +110,57 @@ const hoverBg = (index) => {
 
 const restBg = () => {
     cells.forEach(e => e.classList.remove('hover'));
+}
+
+const checkErr = (value) => {
+    const addErr = (cell) => {
+        if(parseInt(cell.getAtribute('data-value')) !== value){
+            cells.classList.add('err');
+            cells.classList.add('cell-err');
+            setTimeout(() => {
+                cells.classList.remove('cell-err');
+            }, 500);
+        }
+    }
+
+    let index = selected_cell;
+
+    let row = Math.floor(index / CONSTANT.GRID_SIZE);
+    let col = index % CONSTANT.GRID_SIZE;
+
+    let box_start_row = row - row % 3;
+    let box_start_col = col - col % 3;
+
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+            let cell = cells[9 * (box_start_row + i) + (box_start_col + j)];
+            if(!cell.classList.contains('selected')) addErr(cell);
+        }
+    }
+
+    let step = 9;
+    while (index - step >= 0) {
+        addErr(cells[index - step]);
+        step += 9;
+    }
+
+    step = 9;
+    while (index + step < 81) {
+        addErr(cells[index + step]);
+        step += 9;
+    }
+
+    step = 1;
+    while (index - step >= 9*row) {
+        addErr(cells[index - step]);
+        step += 1;
+    }
+
+    step = 1;
+     while (index + step < 9*row + 9) {
+        addErr(cells[index + step]);
+        step += 1;
+    }
 }
 
 const initCellEvent = () => {
